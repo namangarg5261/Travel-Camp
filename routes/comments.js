2ae7af28e2e668b470955708e6f729a1174daddd
namangarg5261 <namangarg5261@gmail.com>
2020-06-08 21:41:11 +0530

    Added flash messages

--- routes/comments.js
+++ routes/comments.js
@@ -19,30 +19,33 @@ router.get("/new",middleware.isLoggedIn, function(req, res){
 
 //Comments Create
 router.post("/",middleware.isLoggedIn,function(req, res){
-   //lookup campground using ID
-   Campground.findById(req.params.id, function(err, campground){
-       if(err){
-           console.log(err);
-           res.redirect("/campgrounds");
-       } else {
-        Comment.create(req.body.comment, function(err, comment){
-           if(err){
-               console.log(err);
-           } else {
-               //add username and id to comment
-               comment.author.id = req.user._id;
-               comment.author.username = req.user.username;
-               //save comment
-               comment.save();
-               campground.comments.push(comment);
-               campground.save();
-               console.log(comment);
-               res.redirect('/campgrounds/' + campground._id);
-           }
-        });
-       }
-   });
-});
+    //lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+         Comment.create(req.body.comment, function(err, comment){
+            if(err){
+                req.flash("error", "Something went wrong");
+                console.log(err);
+            } else {
+                //add username and id to comment
+                comment.author.id = req.user._id;
+                comment.author.username = req.user.username;
+                //save comment
+                comment.save();
+                campground.comments.push(comment);
+                campground.save();
+                console.log(comment);
+                req.flash("success", "Successfully added comment");
+                res.redirect('/campgrounds/' + campground._id);
+            }
+         });
+        }
+    });
+ });
+ 
 
 // COMMENT EDIT ROUTE
 router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req, res){
@@ -66,17 +69,19 @@ router.get("/:comment_id/edit", middleware.checkCommentOwnership, function(req, 
     });
  });
  
- // COMMENT DESTROY ROUTE
- router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, res){
-     //findByIdAndRemove
-     Comment.findByIdAndRemove(req.params.comment_id, function(err){
-        if(err){
-            res.redirect("back");
-        } else {
-            res.redirect("/campgrounds/" + req.params.id);
-        }
-     });
- });
+// COMMENT DESTROY ROUTE
+router.delete("/:comment_id", middleware.checkCommentOwnership, function(req, res){
+    //findByIdAndRemove
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+       if(err){
+           res.redirect("back");
+       } else {
+           req.flash("success", "Comment deleted");
+           res.redirect("/campgrounds/" + req.params.id);
+       }
+    });
+});
+
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -8,30 +8,32 @@ router.get("/", function(req, res){
     res.render("landing");
 });
 
+
 // show register form
 router.get("/register", function(req, res){
-   res.render("register"); 
-});
+    res.render("register", {page: 'register'}); 
+ });
+
 
 //handle sign up logic
 router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
         if(err){
-            req.flash("error", err.message);
-            return res.render("register");
+            console.log(err);
+            return res.render("register", {error: err.message});
         }
         passport.authenticate("local")(req, res, function(){
-           req.flash("success", "Welcome to YelpCamp " + user.username);
+           req.flash("success", "Successfully Signed Up! Nice to meet you " + req.body.username);
            res.redirect("/campgrounds"); 
         });
     });
 });
-
-//show login form
-router.get("/login", function(req, res){
-   res.render("login"); 
-});
+ 
+ //show login form
+ router.get("/login", function(req, res){
+    res.render("login", {page: 'login'}); 
+ });
 
 //handling login logic
 router.post("/login", passport.authenticate("local", 
