4a981c937635331eb76fef29f10d6363c29c6e68
namangarg5261 <namangarg5261@gmail.com>
2020-06-12 15:33:50 +0530

    Changes

--- app.js
+++ app.js
@@ -1,53 +1,31 @@
-// require('dotenv').config();
 var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose"),
-    flash       = require("connect-flash"), 
     passport    = require("passport"),
+    cookieParser = require("cookie-parser"),
     LocalStrategy = require("passport-local"),
-    methodOverride = require("method-override"),
+    flash        = require("connect-flash"),
     Campground  = require("./models/campground"),
     Comment     = require("./models/comment"),
     User        = require("./models/user"),
-    seedDB      = require("./seeds")
+    session = require("express-session"),
+    seedDB      = require("./seeds"),
+    methodOverride = require("method-override");
     
-//requring routes
+//requiring routes
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
-
-// require('dotenv').config()
-
-
-
-// mongoose.connect("mongodb://localhost/yelp_camp");    
-mongoose.connect("mongodb+srv://namangarg5261:Naman@2000@yelpcamp.kkovg.mongodb.net/<dbname>?retryWrites=true&w=majority&authSource=admin", {
-    useNewUrlParser: true,
-    useCreateIndex : true
-}).then(() => {
-    console.log("connected to DB"); 
-}).catch(err => {
-    console.log("ERROR>>>>>>>>>>>>>>>>>>>>>>>>>:", err.message);
-});
-
-// mongoose.connect(PROCESS.ENV.MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useCreateIndex : true
-// }).then(() => {
-//     console.log("connected to DB"); 
-// }).catch(err => {
-//     console.log("ERROR>>>>>>>>>>>>>>>>>>>>>>>>>:", err.message);
-// });
-
-
+    
+mongoose.connect("mongodb://localhost/yelp_camp_v9");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
-app.use(flash());
+app.use(methodOverride('_method'));
+app.use(cookieParser('secret'));
 
-// seedDB();
+// seedDB(); //seed the database
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
@@ -55,6 +33,8 @@ app.use(require("express-session")({
     resave: false,
     saveUninitialized: false
 }));
+
+app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -63,16 +43,17 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
    res.locals.currentUser = req.user;
-   res.locals.error = req.flash("error");
-   res.locals.success = req.flash("success");
+   res.locals.success = req.flash('success');
+   res.locals.error = req.flash('error');
    next();
 });
+
 
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(2000, function(){
-    console.log(`Yelp camp server started at http://localhost:2000/`);
+app.listen(1111, function(){
+    console.log(`Yelp camp server started at http://localhost:1111/`);
 });
