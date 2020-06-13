dd8c8d9e74c22fa15f1488965c7432990270de18
namangarg5261 <namangarg5261@gmail.com>
2020-06-13 14:19:24 +0530

    Changed seed.js

--- app.js
+++ app.js
@@ -16,7 +16,15 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v6");
+mongoose.connect("mongodb://localhost/yelp_camp");
+    mongoose.connect("mongodb+srv://namangarg5261:Naman@2000@yelpcamp.kkovg.mongodb.net/<dbname>?retryWrites=true&w=majority", {
+        useNewUrlParser: true,
+        useCreateIndex : true
+    }).then(() => {
+        console.log("connected to DB"); 
+    }).catch(err => {
+        console.log("ERROR:", err.message);
+    });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
